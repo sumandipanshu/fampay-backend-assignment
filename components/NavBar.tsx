9a890c02d19f2dd97ad3c5a4d2457e30ab6a1e3e
Dipanshu Kumar Suman <dipanshukumarsuman@gmail.com>
2022-10-30 16:56:29 +0530

    added debouncing in the search bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,23 @@
 import { SearchIcon } from "@chakra-ui/icons";
 import { Box, Flex, IconButton, Input } from "@chakra-ui/react";
+import { debounce } from "lodash";
+import { useCallback, useEffect, useState } from "react";
 import { DarkModeSwitch } from "./DarkModeSwitch";
 
-export const NavBar = () => {
+export const NavBar = ({ searchQuery, setSearchQuery }) => {
+  const [query, setQuery] = useState(searchQuery);
+
+  const updateSearchQuery = () => setSearchQuery(query);
+  const handleChange = e => {
+    console.log("query", e.target?.value);
+    setQuery(e.target?.value);
+  };
+  const delayedQuery = useCallback(debounce(updateSearchQuery, 500), [query]);
+  useEffect(() => {
+    delayedQuery();
+    // Cancel the debounce on useEffect cleanup.
+    return delayedQuery.cancel;
+  }, [query, delayedQuery]);
   return (
     <Box
       bg={"gray.200"}
@@ -13,7 +28,13 @@ export const NavBar = () => {
       w={"100%"}
     >
       <Flex h={20} alignItems="center" justifyContent="center">
-        <Input bg={"gray.150"} placeholder="Search" maxW={"40vw"} />
+        <Input
+          bg={"gray.150"}
+          placeholder="Search"
+          maxW={"40vw"}
+          value={query}
+          onChange={handleChange}
+        />
         <IconButton
           bg="gray.400"
           aria-label="Search database"
